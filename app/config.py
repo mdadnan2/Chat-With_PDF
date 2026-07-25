c981861ab4aa99c2a91982ebe086976f938085cf
Mohammad Adnan <mohammadadnanansari9@gmail.com>
2026-07-25 12:13:23 +0530

    initial commit

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str
+    app_version: str
+
+    host: str
+    port: int
+
+    upload_dir: str
+
+    google_api_key: str = ""
+    database_url: str = ""
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
